4c291a049535107ff43ff91729e9cee938c33890
Nick" Byung heon Lee <43138813+smartbh@users.noreply.github.com>
2023-03-02 16:53:43 +0900

    marathon hash 분석

--- marathon.cpp
+++ marathon.cpp
@@ -10,7 +10,7 @@ map : O(log N) n = 맵 원소의 갯수
 unordered_map = O(N)
 
 라이브러리 내부 구현 차이
-map : binary search tree 이중탐색트리
+map : binary search tree 이중탐색트리 
 
 unordered_map : hash table 해시 테이블
 */
@@ -19,15 +19,37 @@ using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {    
     //참가 participant, 완료 completion
+    unordered_map<string, int> complete;
     string answer = "";
+
+    for(auto& i : participant) 
+    {
+        complete[i]++;
+    }
+
+    for(auto& i : completion) 
+    {
+        complete[i]--;
+    }
+
+    for(auto& i : complete)
+    {
+        if( i.second > 0)
+        {
+            answer = i.first;
+            break;
+        }
+    }
+
+    
     return answer;
 }
 
 int main()
 {
     vector<string> participant = {"mislav", "stanko", "mislav", "ana"};
-    vector<string> completion = {};
+    vector<string> completion = {"stanko", "ana", "mislav"};
 
-    solution(participant, completion);
+    cout << solution(participant, completion) << endl;
     return 0;
 }
